fea8287f38c1a76d16186f83979101962ef1327b
GUNGET1 <57786392+GUNGET1@users.noreply.github.com>
2024-04-19 14:19:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const users = {};
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('Connected');
+
+  socket.on('join', (userData) => {
+    const { username, color } = userData;
+    socket.username = username;
+    socket.color = color;
+
+    users[socket.id] = { username, color };
+
+    io.emit('message', `К нам присоединился ${username}`);
+
+    io.emit('updateUsers', users);
+  });
+
+  socket.on('message', (messageData) => {
+    const { message, recipient } = messageData;
+    if (recipient) {
+      const recipientSocket = Object.values(io.sockets.sockets).find((s) => s.username === recipient);
+      if (recipientSocket) {
+        recipientSocket.emit('message', `${socket.username}: ${message}`);
+      }
+    } else {
+      io.emit('message', `${socket.username}: ${message}`);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    const username = socket.username || 'Пользователь';
+    delete users[socket.id];
+    io.emit('message', `${username} покинул чат`);
+    io.emit('updateUsers', users);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
